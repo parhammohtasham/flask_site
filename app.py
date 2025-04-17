f0f6130aabf971a003e2e90f781479947f1c0788
parhammohtasham <206984336+parhammohtasham@users.noreply.github.com>
2025-04-17 12:38:03 -0700

    version1.1
    post.html is uncompleted

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,redirect,request
+from flask_sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.sqlite'
+
+db=SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(100))
+    body=db.Column(db.Text)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/posts')
+def posts():
+    posts=BlogPost.query.all()
+    return render_template('posts.html',posts=posts)
+
+@app.route('/addpost', methods=['GET','POST'])
+def addpost():
+    if request.method=='POST':
+        post_title=request.form['title']
+        post_body=request.form['body']
+        blog_post=BlogPost(title=post_title,body=post_body)
+        db.session.add(blog_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('addpost.html')
+
+
+if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
